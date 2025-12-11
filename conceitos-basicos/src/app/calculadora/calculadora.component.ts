3545b3a62394231157af2cf82d79c366cb06ef24
jeffconexion <jeff.debian2016@gmail.com>
2025-12-11 13:59:29 -0300

    Criação de aplicação calculadora.

---
+++ conceitos-basicos/src/app/calculadora/calculadora.component.ts
@@ -0,0 +1,25 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-calculadora',
+  imports: [FormsModule, CommonModule],
+  templateUrl: './calculadora.component.html',
+  styleUrls: ['./calculadora.component.scss']
+})
+export class CalculadoraComponent {
+
+  valor1: number | null = null;
+  valor2: number | null = null;
+  resultado: number | null = null;
+
+  cacularSoma() {
+    if (typeof this.valor1 === 'number' && typeof this.valor2 === 'number') {
+      this.resultado = this.valor1 + this.valor2;
+    } else {
+      this.resultado = null;
+    }
+  }
+
+}
